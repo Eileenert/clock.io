d1994e92d276cbe432939aad669c219a81c90f91
Eileenert <65852819+Eileenert@users.noreply.github.com>
2020-06-18 14:40:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function showDate(){
+	var now= new Date();            //get all the years,day,weeks,...
+	var year= now.getFullYear();
+	var month=now.getMonth();
+	var todayNumber= now.getDate();
+	var today= now.getDay();
+	var hours= now.getHours();
+	var minutes= now.getMinutes();
+	var seconds= now.getSeconds();
+	
+	if(hours< 10){			//to add a nice time not a 2:3:9 but 02:03:09
+	hours = "0" + hours;}
+	if(minutes< 10){
+    minutes = "0" + minutes ;}
+	if(seconds < 10){
+	var seconds = "0" + seconds ;}
+	
+	
+	document.getElementById("clock").innerHTML= hours+ ":" + minutes + ":" + seconds; //Put the time
+	
+	var monthsName=["January","February","March","April","May","June","July","August","September","October","November","December"];
+	var dayName=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+	
+	document.getElementById("date").innerHTML= dayName[today] +" "+ todayNumber+" "+ monthsName[month]+ " "+ year;
+	
+	
+	setTimeout(showDate, 1000);
+	
+	if (7<hours<20){
+	document.getElementById("night").style.display="none";
+	document.getElementById("day").style.display="flex";
+	document.getElementById("body").style.backgroundColor="#4da6ff";
+	}else{
+	document.getElementById("night").style.display = "flex";
+	document.getElementById("day").style.display="none";
+	document.getElementById("body").style.backgroundColor="#000033";
+	
+	}
+}
+showDate();
+
